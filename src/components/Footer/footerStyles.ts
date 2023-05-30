e1a9ebc78ed59e586904c40e297cdab624abdb81
Reyeslim <reyeslimones@gmail.com>
2023-05-30 11:43:49 +0200

    feat: implemented footer component and styled components

---
+++ src/components/Footer/footerStyles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+export const Container = styled.footer`
+  background: #111;
+  bottom: 0;
+  color: #fff;
+  height: auto;
+  left: 0;
+  right: 0;
+  position: absolute;
+  width: 100vw;
+`;
+
+export const Info = styled.p`
+  font-size: 16px;
+`;
