2bb6686a533c1054d1af4ceb04de51b8d2dcd861
MarioFalcon <maok.seven@gmail.com>
2023-05-30 16:04:25 +0200

    Header y Video

---
+++ src/components/VideoBackground/videoBackgroundStyles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export interface VideoBackgroundProps {
+    videoSrc: string;
+  }
+
+  export const VideoContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Video = styled.video`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
