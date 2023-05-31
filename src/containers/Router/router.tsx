8ebec0f66cc17280d227d4f13d9b249131591de0
MarioFalcon <125299981+MarioFalcon@users.noreply.github.com>
2023-05-31 21:36:29 +0200

    card

--- src/containers/Router/router.tsx
+++ src/containers/Router/router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Dashboard from '../../views/Dashboard/dashboard'
 import Login from '../../views/Auth/Login/login'
 import Signup from '../../views/Auth/Signup/signup'
+import Perfil from '../../views/Perfil/perfil'
 
 const Router: FC = () => {
   return (
@@ -11,6 +12,7 @@ const Router: FC = () => {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/perfil" element={<Perfil />} />
         <Route path="/*" element={<Navigate replace to="/dashboard" />} />
       </Routes>
     </BrowserRouter>
---
+++ src/views/Perfil/perfilStyles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const PerfilContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  height: 300px;
+  padding: 20px;
+  border-radius: 8px;
+  background-color: #f5f5f5;
+  margin: 100px auto;
+`
+
+export const Avatar = styled.div`
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  background-color: #ccc;
+  margin-bottom: 20px;
+`
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`
+
+export const InfoItem = styled.div`
+  margin-bottom: 10px;
+  /* Añade estilos adicionales según tus preferencias */
+`
+
+export const AdditionalContainer = styled.div`
+  background-color: #f5f5f5;
+  padding: 40px;
+  border-radius: 8px;
+  margin: 40px auto;
+  height: 300px;
+`
