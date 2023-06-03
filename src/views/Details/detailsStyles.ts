0024c19a25747d93ec88302e0b668135870b1d3e
MarioFalcon <125299981+MarioFalcon@users.noreply.github.com>
2023-06-03 13:44:19 +0200

    botones nuevos

--- src/views/Details/detailsStyles.ts
+++ src/views/Details/detailsStyles.ts
@@ -5,24 +5,71 @@ export const DetailsContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh; /* El contenedor ocupará el 100% del alto de la pantalla */
-  background-color: #333333; /* Color de fondo gris oscuro */
+  height: 100vh;
+  background-color: #333333;
 `
 
 export const DetailsContent = styled.div`
-  width: 80%; /* Ajusta el ancho según tus necesidades */
+  width: 80%;
   margin-top: 20px;
 `
 
 export const DetailsTitle = styled.h1`
-  color: #ffffff; /* Color del texto blanco */
+  color: #ffffff;
   font-size: 24px;
   margin-bottom: 10px;
-  text-align: center; /* Centra el texto horizontalmente */
+  text-align: center;
 `
 
 export const DetailsExplanation = styled.p`
-  color: #ffffff; /* Color del texto blanco */
+  color: #ffffff;
   font-size: 16px;
-  text-align: center; /* Centra el texto horizontalmente */
+  text-align: center;
+`
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+`
+
+export const Buttoneditar = styled.button`
+  background-color: #3f51b5;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-right: 340px;
+
+  &:hover {
+    background-color: #303f9f;
+  }
+`
+
+export const Buttonfavorito = styled.button`
+  background-color: #3f51b5;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #303f9f;
+  }
+`
+
+export const Buttoneliminar = styled.button`
+  background-color: #3f51b5;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #303f9f;
+  }
 `
