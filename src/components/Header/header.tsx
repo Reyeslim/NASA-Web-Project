e5b5040014d7d8a73f02fbacf68dead915e84fef
Reyeslim <reyeslimones@gmail.com>
2023-06-04 16:14:00 +0200

    feat: implemented logout logic

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,31 +1,31 @@
 import { FC, memo, useCallback } from 'react'
 import { Container, ButtonContainer, Button } from './headerStyles'
 import { useNavigate } from 'react-router-dom'
-
-
+import { logout } from '../../services/firebase/auth'
 
 const Header: FC = () => {
   const handleReset = useCallback(() => {
     console.log('soy reset')
   }, [])
 
-const navigate = useNavigate() 
+  const navigate = useNavigate()
 
-  const handlePerfilClick = ()=> {
-    navigate("/perfil")
+  const handlePerfilClick = () => {
+    navigate('/perfil')
   }
 
- const handleSignup = () => {
-   navigate('/signup')
- }
-
+  const handleLogout = useCallback(async () => {
+    await logout()
+    navigate('/login')
+    console.log('cerramos')
+  }, [navigate])
 
   return (
     <Container>
       <ButtonContainer>
         <Button onClick={handleReset}>RESET</Button>
         <Button onClick={handlePerfilClick}>PERFIL</Button>
-        <Button onClick={handleSignup}>CERRAR</Button>
+        <Button onClick={handleLogout}>CERRAR</Button>
       </ButtonContainer>
     </Container>
   )
